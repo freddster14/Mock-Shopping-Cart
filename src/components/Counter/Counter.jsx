807fc3276625441d39a5068c2d6fe7c19463d974
Zeronicc <zeronic4465@gmail.com>
2025-01-25 00:49:16 -0600

    update: organized Counter file and modified its css

--- src/components/Counter/Counter.jsx
+++ src/components/Counter/Counter.jsx
@@ -6,36 +6,44 @@ function Counter({
     setQuantity,
  }) {
 
-    function quantityEvent(target) {
-        let { value, min, max } = target;
-        value = Math.max(Number(min), Math.min(Number(max), Number(value)));
-        if(isNaN(value)) return
-        setQuantity({ value });
-    }
-    //Button increment / decrement logic
-    function modifyQuantity(value, input) {
-        value > 0 ? input.value -= -1 : input.value -= 1   
-        quantityEvent(input)
-    }
-    return (
-        <div className={styles.container}>
-          <button className={styles.minus} onClick={(e) => modifyQuantity(-1, e.target.nextElementSibling)}>−</button>
-          <input 
-          className={styles.input} 
-          value={quantity}
-          onChange={(e) => quantityEvent(e.target)}
-          placeholder={quantity} 
-          min="0" 
-          max="99" 
-          type="text" />
-          <button className={styles.plus} onClick={(e) => modifyQuantity(1, e.target.previousElementSibling)}>+</button>
-        </div> 
-    )
+  function quantityEvent(target) {
+      let { value, min, max } = target;
+      value = Math.max(Number(min), Math.min(Number(max), Number(value)));
+      if(isNaN(value)) return
+      setQuantity({ value });
+  }
+  //Button increment / decrement logic
+  function modifyQuantity(value, input) {
+      value > 0 ? input.value -= -1 : input.value -= 1   
+      quantityEvent(input)
+  }
+  return (
+    <div className={styles.container}>
+      <button 
+        className={styles.minus} 
+        onClick={(e) => modifyQuantity(-1, e.target.nextElementSibling)}>
+        −
+      </button>
+      <input 
+        className={styles.input} 
+        value={quantity}
+        onChange={(e) => quantityEvent(e.target)}
+        placeholder={quantity} 
+        min="0" 
+        max="99" 
+        type="text" />
+      <button 
+        className={styles.plus} 
+        onClick={(e) => modifyQuantity(1, e.target.previousElementSibling)}>
+        +
+      </button>
+    </div> 
+  )
 }
 
 Counter.propTypes = {
-    quantity: PropTypes.number,
-    setQuantity: PropTypes.func,
+  quantity: PropTypes.number,
+  setQuantity: PropTypes.func,
 }
 
 export default Counter
